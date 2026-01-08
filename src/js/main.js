ec968cddb0fce91dc80c7718a089a6e41c1a8134
Eilith404 <minaya.sofia@gmail.com>
2026-01-08 13:01:37 +0100

    Add click functionality to all Pokémon buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
-'use strict';
+"use strict";
 
 // SECCIÓN DE QUERY-SELECTOR
-// Éstos son los elementos que nos traemos de la página HTML y usamos en el código
-
-
+const pokemonButtons = document.querySelectorAll(".js_pokemonBtn");
+const modal = document.querySelector(".js_modal");
+const modalCloseBtn = document.querySelector(".js_modalCloseBtn");
 
 // SECCIÓN DE DATOS
 // Variables globales que almacenan la información principal de la aplicación
 // y se usan por todo el fichero.
-
-
 
 // SECCIÓN DE FUNCIONES
 // Éstas son funciones:
@@ -17,23 +15,23 @@
 //   - con código que usaremos en los eventos
 //   - para pintar (render) en la página.
 
-
-
 // SECCIÓN DE FUNCIONES DE EVENTOS
 // Aquí van las funciones handler/manejadoras de eventos
 
-
-
 // SECCIÓN DE EVENTOS
-// Éstos son los eventos a los que reacciona la página
-// Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
-
-
+pokemonButtons.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+  });
+});
+modalCloseBtn.addEventListener("click", (ev) => {
+  modal.classList.add("hidden");
+  modal.classList.remove("visible");
+});
 
 // SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
 // Este código se ejecutará cuando se carga la página
 // Lo más común es:
 //   - Pedir datos al servidor
 //   - Pintar (render) elementos en la página
-
-console.log('Página y JS cargados!');
